b35f0a84ce71cfe5b91e449cf0a61cb1edfbbc05
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-14 16:01:31 -0700

    populate reducers

---
+++ src/reducers/post-list-reducer.jsx
@@ -0,0 +1,16 @@
+import * as c from './../actions/ActionTypes';
+
+export default (state = {}, action) => {
+  const { id } = action;
+
+  switch (action.type) {
+
+    case c.DELETE_POST:
+      const newState = { ...state };
+      delete newState[id];
+      return newState;
+
+    default:
+      return state;
+  }
+};
