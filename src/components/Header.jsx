c7351b124fc51891e3c854c1c213d027742002cb
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-14 15:48:38 -0700

    File structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-routerdom";
+import styled from "styled-components";
+
+const ProfileShellHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: white;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: black;
+`;
+
+function Header() {
+  return (
+    <StyledWrapper>
+      <>
+        <ProfileShellHeader>
+          Profile Shell
+        </ProfileShellHeader>
+        <ul>
+          <li>
+            <Link to = "/">Home</Link>
+          </li>
+          <li>
+            <Link to = "/signin">Sign In</Link>
+          </li>
+        </ul>
+      </>
+    </StyledWrapper>
+  );
+}
