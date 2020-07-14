bf924381b2459244d1fb09aa4fed091344ae7767
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-14 15:13:46 -0700

    Firebase connected & continued scafolding

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import UserControl from "./UserControl";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path = "/signin">
+          <Signin />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
