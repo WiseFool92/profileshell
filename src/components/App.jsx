ac2dc127edc3b9154915f3c3d02986d21976dddc
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-14 15:57:44 -0700

    populate app & edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import UserControl from "./UserControl";
+import PostControl from "./PostControl";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
---
+++ src/components/EditPostForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+import { useFirestore } from 'react-redux-firebase';
+
+function EditPostForm (props) {
+  const firestore = useFirestore();
+  const { post } = props;
+
+  function handleEditPostFormSubmission(event) {
+    event.preventDefault();
+    props.onEditPost();
+    const propertiesToUpdate = {
+      title: event.target.title.value, 
+      subject: event.target.subject.value, 
+      description: event.target.description.value
+    }
+    return firestore.update({collection: 'posts', doc: post.id },propertiesToUpdate)
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditPostFormSubmission}
+        buttonText="Update" />
+    </React.Fragment>
+  );
+}
+
+EditPostForm.propTypes = {
+  onEditPost: PropTypes.func
+};
+
+export default EditPostForm;
